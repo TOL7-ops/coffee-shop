1800d14574265043d75e638d5c1c638012f84c7b
TOL7-ops <praisedilord0@gmail.com>
2025-10-30 16:31:34 -0700

    we keep pushing

---
+++ js/script.js
@@ -0,0 +1,39 @@
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+    grabCursor: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        }
+    },
+});
+
+
+// for hamburger
+
+const menu = document.querySelector('#menu-btn');
+const navBar = document.querySelector('.nav-bar');
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle('fa-times'); // switch bars → X icon
+  navBar.classList.toggle('active'); // show/hide nav
+});
+// when a hamburger link is clicked
+document.querySelectorAll('.nav-bar a').forEach(link => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('fa-times');
+    navBar.classList.remove('active');
+  });
+});
